d57d2e9b7f4ad504515f959e08d471ff55bc50e8
Chris Price <christopher.r.price@gmail.com>
2020-02-13 01:12:25 -0500

    Initial commit.

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Chris Price on 2/13/20.
+//  Copyright © 2020 Chris Price. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
